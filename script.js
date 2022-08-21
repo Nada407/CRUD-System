fe5318d84b38c301201ee633fb1eb8a57b86898e
Nada407 <nadasharif775@gmail.com>
2022-08-21 14:06:58 +0200

    First commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var productContainer = []; //We made here array of Objects to store all the data and to avoid the overwriting
+var produproductPriceInputctContainer = []; //We made here array of Objects to store all the data and to avoid the overwriting
 if (localStorage.getItem("OurProducts") != null) {
   productContainer = JSON.parse(localStorage.getItem("OurProducts")); //3ashan matrg3sh str trg3 array
   displayProducts();
 }
 var productNameInput = document.getElementById("productName");
-var productPriceInput = document.getElementById("productPrice");
+var productPriceInput   = document.getElementById("productPrice");
 var productCategoryInput = document.getElementById("productCategory");
 var productDescriptionInput = document.getElementById("productDescription");
 
@@ -19,6 +19,7 @@ function addProduct() {
   productContainer.push(product); // to avoid that elements will overwrite on each other , and put it in box(Container)
   localStorage.setItem("OurProducts", JSON.stringify(productContainer)); //to put the items in local storage after adding from the user side
   clearForm();
+  displayProducts();
 }
 function clearForm() {
   productNameInput.value = "";
@@ -36,7 +37,7 @@ function displayProducts() {
         <td>${productContainer[i].price}</td>
         <td>${productContainer[i].category}</td>
         <td>${productContainer[i].description}</td>
-       <td><button class="btn btn-outline-warning">Update</button></td>
+       <td><button onclick='updateProducts(${i})'  class="btn btn-outline-warning">Update</button></td>
         <td><button onclick='deleteproduct(${i})' class="btn btn-outline-danger">Delete</button></td>
         
         </tr>`;
@@ -59,7 +60,8 @@ function searchProduct(term) {
   var cartona = ``;
   for (var i = 0; i < productContainer.length; i++) {
     //this is how i go throug all the elements in the table
-if (productContainer[i].name.toLowerCase().includes(term.toLowerCase()) == true) {
+    if (
+      productContainer[i].name.toLowerCase().includes(term.toLowerCase())==true) {
       //if the list of  productcontainer names has the term that has been selected
       //to generlaize the cases will make all the letters in lower case
       //Now we want only to show the similar data that appears on search
@@ -71,15 +73,32 @@ if (productContainer[i].name.toLowerCase().includes(term.toLowerCase()) == true)
         <td>${productContainer[i].price}</td>
         <td>${productContainer[i].category}</td>
         <td>${productContainer[i].description}</td>
-       <td><button class="btn btn-outline-warning">Update</button></td>
+       <td><button onclick='updateProducts(${i})' class="btn btn-outline-warning">Update</button></td>
         <td><button onclick='deleteproduct(${i})' class="btn btn-outline-danger">Delete</button></td>
         
         </tr>`;
     }
-    
-    document.getElementById("tableBody").innerHTML = cartona;
+
   }
+  document.getElementById("tableBody").innerHTML = cartona;
+
 
   //Using String Methods to generlaize the lower case and select only the item that appears in search
+  searchProduct();
+
 }
-searchProduct();
+
+function updateProducts(index) {//show the data
+    productNameInput.value=productContainer[index].name;
+    productPriceInput.value=productContainer[index].price;
+    productCategoryInput.value=productContainer[index].category;
+    productDescriptionInput.value=productContainer[index].description;
+   document.getElementById("mainBtn").innerHTML="Update Product"
+
+}
+function updateProoducts(index){///update from the html button
+productContainer.splice(index, 1);
+localStorage.setItem("OurProducts", JSON.stringify(productContainer));
+addProduct();
+}
+
